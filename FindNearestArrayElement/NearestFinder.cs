712ee9d5c195fd30649e0415faccff5262d78258
Igor Gorskyi <igorsky@igorsky.net>
2025-07-28 20:40:11 +0100

    Added summary

--- FindNearestArrayElement/NearestFinder.cs
+++ FindNearestArrayElement/NearestFinder.cs
@@ -1,7 +1,20 @@
 namespace ConsoleApp1;
-
+/// <summary>
+/// Provides a method for efficiently finding the closest integer to a target in a sorted array.
+/// </summary>
 public static class NearestFinder
 {
+    /// <summary>
+    /// Finds the integer in a sorted array that is closest to the specified target.
+    /// </summary>
+    /// <param name="array">A sorted array of integers (in ascending order).</param>
+    /// <param name="target">The target value to compare against.</param>
+    /// <returns>
+    /// The integer from the array that is numerically closest to the target.
+    /// If two values are equally close, the smaller value is returned.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">Thrown if the array is null.</exception>
+    /// <exception cref="ArgumentException">Thrown if the array is empty.</exception>
     public static int FindNearest(int[] array, int target)
     {
         if (array == null)
